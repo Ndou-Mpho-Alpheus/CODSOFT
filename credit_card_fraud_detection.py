1a2f0d7895136418cd3eb2c86b1f863cc2218f88
Ndou Mpho Alpheus <ndoumpho01@gmail.com>
2024-06-17 00:37:10 +0200

    Initial commit

---
+++ credit_card_fraud_detection.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from imblearn.over_sampling import SMOTE
+import matplotlib.pyplot as plt
+import seaborn as sns
